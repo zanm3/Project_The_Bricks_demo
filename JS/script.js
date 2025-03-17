1bce3b2f790e06101e100cece99478e9248c3620
zanm3 <zan.markuza@tscng.org>
2025-03-17 10:18:17 +0100

    update

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,67 @@
-// koda pobrana iz učilnice
-
-function drawBall(){
+ function draw() {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-
-    // narišemo žogico
-
+    //draw a circle
     ctx.beginPath();
-    ctx.arc(75,75,10,0,Math.PI*2,true);
+    ctx.arc(75, 75, 10, 0, Math.PI*2, true);
     ctx.closePath();
     ctx.fill();
-}
 
-function ballMovement(){
+    clear();
+    
+    circle(x, y, 10);
+    
+    if (x + dx > WIDTH (-r)|| x + dx < 0 (+r))
+    
+    dx = -dx;
+    
+    if (y + dy > HEIGHT (-r)|| y + dy < 0 (+r))
+    
+    dy = -dy;
+    
+    x += dx;
+    
+    y += dy;
+}
+function drawIt() {
     let x = 150;
     let y = 150;
     let dx = 2;
     let dy = 4;
-    let canvas;
     let ctx;
-    function init(){
-        canvas = document.getElementById("canvas");
-        ctx = canvas.getContext("2d");
-        return setInterval(drawBall, 10) // klic funkcije draw vsakih 10 ms;
+    let canvas
+    function init() {
+    
+    canvas=document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    return setInterval(draw, 10); //klic funkcije draw vsakih 10 ms; http://www.w3schools.com/jsref/met_win_setinterval.asp
     }
-    function drawBall(){
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        ctx.beginPath();
-        ctx.arc(x,y,10,0,Math.PI*2,true);
-        ctx.closePath();
-        ctx.fill();
-        x += dx;
-        y += dy;
+    function draw() {
+    ctx.clearRect(0,0,300,300);
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.fill();
+    x += dx;
+    y += dy;
     }
     init();
-}
 
+    clear();
+    
+    circle(x, y, 10);
+    
+    if (x + dx > WIDTH (-r)|| x + dx < 0 (+r))
+    
+    dx = -dx;
+    
+    if (y + dy > HEIGHT (-r)|| y + dy < 0 (+r))
+    
+    dy = -dy;
+    
+    x += dx;
+    
+    y += dy;
+}
+window.onload(draw());
+window.onload(drawIt()); 
