e96e9a720adf465f8b6c1525241e15fdfdfb9228
zanm3 <zan.markuza@tscng.org>
2025-03-17 10:45:50 +0100

    update

--- JS/script.js
+++ JS/script.js
@@ -1,35 +1,21 @@
- function draw() {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
+function draw() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    
     //draw a circle
     ctx.beginPath();
     ctx.arc(75, 75, 10, 0, Math.PI*2, true);
     ctx.closePath();
     ctx.fill();
-
-    clear();
     
-    circle(x, y, 10);
-    
-    if (x + dx > WIDTH (-r)|| x + dx < 0 (+r))
-    
-    dx = -dx;
-    
-    if (y + dy > HEIGHT (-r)|| y + dy < 0 (+r))
-    
-    dy = -dy;
-    
-    x += dx;
-    
-    y += dy;
 }
 function drawIt() {
-    let x = 150;
-    let y = 150;
-    let dx = 2;
-    let dy = 4;
-    let ctx;
-    let canvas
+    var x = 150;
+    var y = 150;
+    var dx = 2;
+    var dy = 4;
+    var ctx;
+    var canvas
     function init() {
     
     canvas=document.getElementById('canvas');
@@ -46,22 +32,8 @@ function drawIt() {
     y += dy;
     }
     init();
+    }
 
-    clear();
-    
-    circle(x, y, 10);
-    
-    if (x + dx > WIDTH (-r)|| x + dx < 0 (+r))
-    
-    dx = -dx;
-    
-    if (y + dy > HEIGHT (-r)|| y + dy < 0 (+r))
-    
-    dy = -dy;
-    
-    x += dx;
-    
-    y += dy;
-}
-window.onload(draw());
-window.onload(drawIt()); 
+document.addEventListener("onkeypress", (e) => {
+    window.alert(e.data);
+});
